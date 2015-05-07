8b462d563e4793b7642a6b7bc2f5ab2d19246eb1
Marco Chang <imnotmarco@live.com>
2015-05-07 10:51:49 -0400

    Integrated BooBoo

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -1,4 +1,7 @@
 <?php
+require './vendor/autoload.php';
+
+use BooBoo\BooBoo;
 /**
  * CodeIgniter
  *
@@ -132,9 +135,10 @@ if ( ! is_php('5.4'))
  *  Define a custom error handler so we can log PHP errors
  * ------------------------------------------------------
  */
-	set_error_handler('_error_handler');
+	/*set_error_handler('_error_handler');
 	set_exception_handler('_exception_handler');
-	register_shutdown_function('_shutdown_handler');
+	register_shutdown_function('_shutdown_handler');*/
+	BooBoo::setUp();
 
 /*
  * ------------------------------------------------------
