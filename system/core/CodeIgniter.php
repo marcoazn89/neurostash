2bca61e4ef5e86124848ba01b13919cdbba813bc
Marco Chang <imnotmarco@live.com>
2015-05-07 11:54:20 -0400

    added BooBoo again

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -1,4 +1,8 @@
 <?php
+require './vendor/autoload.php';
+
+use BooBoo\BooBoo;
+
 /**
  * CodeIgniter
  *
@@ -132,9 +136,10 @@ if ( ! is_php('5.4'))
  *  Define a custom error handler so we can log PHP errors
  * ------------------------------------------------------
  */
-	set_error_handler('_error_handler');
+	BooBoo::setUp();
+	/*set_error_handler('_error_handler');
 	set_exception_handler('_exception_handler');
-	register_shutdown_function('_shutdown_handler');
+	register_shutdown_function('_shutdown_handler');*/
 
 /*
  * ------------------------------------------------------
